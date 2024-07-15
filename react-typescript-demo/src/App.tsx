fdea2034790a99d37f2a222c0e10451d820f11ee
Ekrem <ekremteke.uk@gmail.com>
2024-07-15 14:03:51 +0100

    handle click button was added

--- react-typescript-demo/src/App.tsx
+++ react-typescript-demo/src/App.tsx
@@ -8,28 +8,24 @@ import { AppProps, Users } from './App.types';
 const App: FC<AppProps> = ({ title }) => {
   const [users, setUsers] = useState<Users[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  useEffect(() => {
-    const getUsers = async () => {
-      try {
-        setIsLoading(true);
-        const { data } = await axios.get(
-          'https://randomuser.me/api/?results=10'
-        );
-        console.log(data);
-        setUsers(data.results)
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setIsLoading(false)
-      }
 
-    };
-    getUsers();
-  }, []);
+  const handleClick = async () => {
+    try {
+      setIsLoading(true);
+      const { data } = await axios.get('https://randomuser.me/api/?results=10');
+      console.log(data);
+      setUsers(data.results);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <div>
       <h1>{title}</h1>
+      <button onClick={handleClick}>Show Users </button>
       {isLoading && <p>Loading...</p>}
       <ul>
         {users.map(({ login, name, email }) => {
